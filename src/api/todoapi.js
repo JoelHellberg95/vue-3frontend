4fb69178b0c59e4ed41a98265d47f55c30b17b86
Joel Hellberg <118288883+JoelHellberg95@users.noreply.github.com>
2025-04-29 13:21:45 +0200

    Updated the todo app with toast and a better loading and crud

--- src/api/todoapi.js
+++ src/api/todoapi.js
@@ -1,26 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://localhost:44322/api/todo',
-    headers:{
+    baseURL: 'https://todoapi-dq99.onrender.com/api/todo', // ✅ Your Render API
+    headers: {
         'Content-Type': 'application/json',
-    }
+    },
 });
 
 export default {
-    getAll() {
-        return api.get('/');
-    },
-    get(id) {
-        return api.get(`/${id}`);
-    },
-    create(todo) {
-        return api.post('/', todo);
-    },
-    update(id, todo) {
-        return api.put(`/${id}`, todo);
-    },
-    delete(id) {
-        return api.delete(`/${id}`);
-    },
+    getAll: () => api.get('/'),
+    create: (todo) => api.post('/', todo),
+    update: (id, todo) => api.put(`/${id}`, todo),
+    delete: (id) => api.delete(`/${id}`),
 };
